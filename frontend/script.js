f302423b451875385901fd89cfa5af3d5c6a711d
chaibo <118512110+chaibo9@users.noreply.github.com>
2026-02-26 17:19:06 -0500

    front-end setup

---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+// term pop-up button
+function showTermModal() {
+    document.getElementById("termModal").style.display = "block";
+}
+
+function hideTermModal() {
+    document.getElementById("termModal").style.display = "none";
+}
+
+// course pop-up button
+function showCourseModal() {
+    document.getElementById("courseModal").style.display = "block";
+}
+
+function hideCourseModal() {
+    document.getElementById("courseModal").style.display = "none";
+}
